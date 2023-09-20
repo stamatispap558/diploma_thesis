ba032dc6830fb37eb49513318f67b3944700ee31
root <root@dev12.myguest.virtualbox.org>
2023-09-20 18:16:31 +0600

    updated

--- exim-frontend/src/Pages/Auth/SignUp.js
+++ exim-frontend/src/Pages/Auth/SignUp.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +12,8 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import axios from "axios";
+
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -21,11 +23,9 @@ export default function SignUp() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    console.log(data);
   };
+
 
   return (
     <ThemeProvider theme={defaultTheme}>
